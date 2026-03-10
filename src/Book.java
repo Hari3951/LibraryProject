4be7d4f63016c0827ca4b9b5c55a5693d582aa10
Hariharan Senthilkumar <53120475+Hari3951@users.noreply.github.com>
2026-03-09 18:03:08 -0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -7,142 +7,139 @@ public class Book {
     private String title;
     private String author;
     private String genre;
-    //private boolean isCheckOut;
     private double rating;
     private String series;
-    
+    private boolean checkedOut;
+
     private static int totalBooks = 0;
 
-    // Constructor for book class would go here
-	///uhhhh okay i didn't see all the stuff in minilibrary so some of these constructors need updating
-    public Book() { //literally just a placeholder so that the code can compile in the driver
-    	this.title = "";
-    	this.author = "";
-    	this.genre = "UNLABELED";
-    	this.rating = -1.0;
-    	this.series = "Standalone";
+    public Book() {
+        this.title = "";
+        this.author = "";
+        this.genre = "UNLABELED";
+        this.rating = -1.0;
+        this.series = "Standalone";
+        this.checkedOut = false;
+    }
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.genre = "UNLABELED";
+        this.rating = -1.0;
+        this.series = "Standalone";
+        this.checkedOut = false;
+        totalBooks++;
+    }
+
+    public Book(String title, String author, String genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.rating = -1.0;
+        this.series = "Standalone";
+        this.checkedOut = false;
+        totalBooks++;
+    }
+
+    public Book(String title, String author, String genre, double rating, String series) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.rating = rating;
+        this.series = series;
+        this.checkedOut = false;
+        totalBooks++;
+    }
+
+    public Book(Book book) {
+        this.title = book.getTitle();
+        this.author = book.getAuthor();
+        this.genre = book.getGenre();
+        this.rating = book.getRating();
+        this.series = book.getSeries();
+        this.checkedOut = book.isCheckedOut();
+        totalBooks++;
+    }
+
+    public void checkOut() {
+        checkedOut = true;
+    }
+
+    public void checkIn() {
+        checkedOut = false;
+    }
+
+    public boolean rate(double newRating) {
+        if (newRating < 0.0 || newRating > 10.0) {
+            return false;
+        }
+        rating = newRating;
+        return true;
+    }
+
+    public ArrayList<Book> findSiblings(MiniLibrary library) {
+        ArrayList<Book> libraryInventory = library.getInventory();
+        ArrayList<Book> siblings = new ArrayList<Book>();
+
+        if (this.getSeries().toLowerCase().equals("standalone")) {
+            return siblings;
+        }
+
+        for (Book book : libraryInventory) {
+            if (!this.equals(book) &&
+                book.getSeries().toLowerCase().equals(this.getSeries().toLowerCase())) {
+                siblings.add(book);
+            }
+        }
+        return siblings;
+    }
+
+    public String getTitle() {
+        return title;
     }
     
-    
-    public Book(String title, String author) {
-		this.title = title;
-		this.author = author;
-		this.genre = "UNLABELED";
-		this.rating = -1.0; 
-		this.series = "Standalone";
-        //this.isCheckOut = false;
-		totalBooks++;
-	}
-	
-	public Book(String title, String author, String genre) {
-		this.title = title;
-		this.author = author;
-		this.genre = genre;
-		this.rating = -1.0;
-		this.series = "Standalone";
-        //this.isCheckOut = false;
-		totalBooks++;
-	}
-
-	public Book(String title, String author, String genre, double rating, String series) {
-		this.title = title;
-		this.author = author;
-		this.genre = genre;
-		this.rating = rating;
-		this.series = series;
-        //this.isCheckOut = false;
-		totalBooks++;
-	}
-	
-	public Book(Book book) {
-		this.title = book.getTitle();
-		this.author = book.getAuthor();
-		this.genre = book.getGenre();
-		this.rating = book.getRating();
-		this.series = book.getSeries();
-        //this.isCheckOut = false;
-		totalBooks++;
-	}
-	
-    // Method for Checking out a book
-//    public void checkOut(){
-//        isCheckOut = true;
-//    }
-//    
-//    // Method for Checking in a book
-//    public void checkIn(){
-//        isCheckOut = false;
-//    }
-    
-    //(if time) method for rating a book
-    
-	//for the sake of preserving mental strength, assume preconditions are met
-	//preconditions: library is not null
-	public ArrayList<Book> findSiblings(MiniLibrary library){
-		ArrayList<Book> libraryInventory = library.getInventory();
-		ArrayList<Book> siblings = new ArrayList<Book>();
-		
-		if (this.getSeries().toLowerCase().equals("standalone")) {
-			return siblings;
-		}
-		
-		for (Book book : libraryInventory) {
-			if (!this.equals(book) &&
-					book.getSeries().toLowerCase().equals(this.getSeries().toLowerCase())){
-				siblings.add(book);
-			}
-		}
-		return siblings;
-	}
-	
-	
-    // Getters for title, author, genre, rating, etc.
-    public String getTitle() {
-		return title;
-	}
-    
     public String getAuthor() {
-		return author;
-	}
+        return author;
+    }
 
     public String getGenre() {
-		return genre;
-	}
+        return genre;
+    }
 
     public double getRating() {
-    	return rating;
+        return rating;
     }
     
     public String getSeries() {
-    	return series;
+        return series;
+    }
+
+    public boolean isCheckedOut() {
+        return checkedOut;
+    }
+
+    public void setCheckedOut(boolean status) {
+        checkedOut = status;
     }
     
     public static int getTotalBooks() {
-    	return totalBooks;
+        return totalBooks;
     }
-//    public boolean isCheckedOut() {
-//        return isCheckOut;
-//    }
-//
-//	public void setCheckedOut(boolean status) {
-//         this.isCheckOut = status;
-//    }
 
-    
-    // Method for displaying a single book 
     public String toString() {
-		String bookString = "\""+this.title+"\""+" by "+this.author+" ("+genre+")";
-		return bookString;
-	}
+        String status = "Available";
+        if (checkedOut) {
+            status = "Checked Out";
+        }
 
-    //equals method
+        return "\"" + title + "\" by " + author +
+               " (" + genre + ", " + series + ", rating: " + rating + ", " + status + ")";
+    }
+
     public boolean equals(Book otherBook) {
-		boolean titleMatch = this.title.equals(otherBook.getTitle());
-		boolean authorMatch = this.author.equals(otherBook.getAuthor());
-		
-		if (titleMatch && authorMatch) return true; 
-		else return false;
-	}
-    
-
+        boolean titleMatch = this.title.equalsIgnoreCase(otherBook.getTitle());
+        boolean authorMatch = this.author.equalsIgnoreCase(otherBook.getAuthor());
+        return titleMatch && authorMatch;
+    }
 }
